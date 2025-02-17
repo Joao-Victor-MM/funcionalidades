197bb7593ff9796bdd54d298784c38bca99f9b29
João Victor Monção <joao.victor.moncaomatos@gmail.com>
2025-02-17 14:42:54 -0300

    adicionando o aparecimento do menu de configurações ao clicar no botao

--- funcionalidades.js
+++ funcionalidades.js
@@ -2,10 +2,17 @@ let bola = document.getElementById("bola")
 let trilho = document.getElementById("botão-trilho")
 let body = document.querySelector('body')
 
+let configbutton = document.getElementById("config-menu")
+let configmenu = document.getElementById("configurações")
 
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle("light")
     bola.classList.toggle("light")
     body.classList.toggle("light")
  
+})
+
+configbutton.addEventListener('click', ()=>{
+    configbutton.classList.toggle("openmenu")
+    configmenu.classList.toggle("openmenu")
 })
